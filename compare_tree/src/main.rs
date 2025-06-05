31fcfa8063c59608bf15e7c4d3b5d4a5d467f252
Julien Thevenon <julien_thevenon@yahoo.fake.fr>
2025-06-05 18:02:59 +0200

    Check for number of arguments

--- compare_tree/src/main.rs
+++ compare_tree/src/main.rs
@@ -1,7 +1,13 @@
 use std::env::args;
+use std::process;
 
 fn main() {
     println!("Hello, world!");
     let args: Vec<String> = args().collect();
-    dbg!(args);
+    dbg!(&args);
+
+    if args.len() < 3 {
+        println!("Waiting for 2 arguments");
+        process::exit(-1);
+    }
 }
