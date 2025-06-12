fd699f5a98895e13bb94c5357c064d5f2fb1b6d1
Julien Thevenon <julien_thevenon@yahoo.fake.fr>
2025-06-12 08:40:13 +0200

    Add tests for parsing

--- compare_tree/src/lib.rs
+++ compare_tree/src/lib.rs
@@ -6,6 +6,8 @@ pub fn run(configuration: &Config) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
+#[derive(PartialEq)]
+#[derive(Debug)]
 pub struct Config {
     reference_path: String,
     other_path: String
@@ -19,5 +21,26 @@ impl Config {
         let reference_path = args[1].clone();
         let other_path = args[2].clone();
         Ok(Config {reference_path, other_path})
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_parse() {
+        let args = ["command".to_string(), "reference".to_string(), "other".to_string()];
+        let ref_config = Config {
+            reference_path: "reference".to_string(),
+            other_path: "other".to_string()
+        };
+        let result = Config::build(&args).unwrap();
+        assert_eq!(ref_config, result);
+    }
+    #[test]
+    fn test_parse_fail() {
+        let args = ["reference".to_string(), "other".to_string()];
+        assert!(Config::build(&args).is_err());
     }
 }
