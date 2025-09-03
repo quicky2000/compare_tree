188fd111d95caf1b6c192d741e31fd2fc672d652
Julien Thevenon <julien_thevenon@yahoo.fake.fr>
2025-09-03 13:03:36 +0200

    Do some cleaning in run function

--- compare_tree/src/lib.rs
+++ compare_tree/src/lib.rs
@@ -140,14 +140,6 @@ pub fn run(configuration: &Config) -> Result<(), Box<dyn Error>> {
     println!("Reference path {}", configuration.reference_path);
     println!("Other path {}", configuration.other_path);
 
-    let my_info = filetree_info::FileTreeInfo {
-        name: "my_filetree".to_string(),
-        height: 8,
-        sha1: sha1::compute_sha1(vec!(0)),
-        nb_item: 13
-    };
-    println!("{}", my_info);
-
     let result = check_directory(&configuration.reference_path);
     if result.is_err() {
         return Err(result.err().unwrap().into());
@@ -158,56 +150,19 @@ pub fn run(configuration: &Config) -> Result<(), Box<dyn Error>> {
     }
 
     let analyse_result = analyse(&configuration.reference_path);
-    let analyse = match analyse_result {
+    let analysed = match analyse_result {
         Ok(k) => k,
         Err(e) => return Err(e.into())
     };
-    println!("{}", analyse);
+    println!("Reference : {}", analysed);
 
-    let key_result = compute_file_sha1(&configuration.reference_path);
-    let key = match key_result {
+    let analyse_result = analyse(&configuration.other_path);
+    let analysed = match analyse_result {
         Ok(k) => k,
         Err(e) => return Err(e.into())
     };
-    println!("Key is {key}");
+    println!("Other : {}", analysed);
 
-    let check = fs::exists(&configuration.other_path);
-    if check.is_err() || !check.unwrap() {
-        return Err(format!("file {} do not exist", configuration.other_path).into())
-    }
-    let metadata_result = fs::symlink_metadata(&configuration.other_path);
-    if metadata_result.is_err() {
-        return Err(format!("Unable to collect metadata from file {}", configuration.other_path).into());
-    }
-    let metadata = metadata_result.unwrap();
-    if metadata.is_dir() {
-        println!("{} is a directory", configuration.other_path);
-        let dir_iter_result = fs::read_dir(&configuration.other_path);
-        let dir_iter = match dir_iter_result {
-            Ok(dir_iter) => dir_iter,
-            Err(_e) => return Err(format!("problem with dir_iter on {}", configuration.other_path).into())
-        };
-        for item_result in dir_iter {
-            let item = match item_result {
-                Ok(item) => item,
-                Err(_e) => return Err(format!("Issue with item").into())
-            };
-            println!("=> {}", item.path().display());
-        }
-
-    }
-    if metadata.is_file() {
-        println!("{} is a file", configuration.other_path)
-    }
-    if metadata.is_symlink() {
-        println!("{} is a link", configuration.other_path)
-    }
-    let key_result = compute_link_sha1(&configuration.other_path);
-    let key = match key_result {
-        Ok(k) => k,
-        Err(e) => return Err(e.into())
-    };
-    println!("Key is {key}");
     Ok(())
 }
 
